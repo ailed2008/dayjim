cdbf7daf44b933b637dd9116a90df9f9598d6188
JuanENP <juanjose_skorg@hotmail.com>
2021-10-30 21:36:11 -0500

    Change_principal_and_blog

--- js/index.js
+++ js/index.js
@@ -28,15 +28,14 @@ function scrollActive(){
     const scrollY = window.pageYOffset
 
     sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
+        const sectionHeight = current.offsetHeight;
         const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+        var  sectionId = current.getAttribute('id');
         
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav_menu a[href*='+sectionId+']').classList.add('active')
-        }
-        else{
-            document.querySelector('.nav_menu a[href*='+sectionId+']').classList.remove('active')
+            document.querySelector('.nav_menu a[href*=' + sectionId+']').classList.add('active')
+        }else{
+            document.querySelector('.nav_menu a[href*=' + sectionId+']').classList.remove('active')
         }
     })
 }
